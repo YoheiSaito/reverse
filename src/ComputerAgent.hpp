a947922028e1e59dfac66233baffecca510ef38e
YoheiSaito <teyo.13324@gmail.com>
2019-03-20 11:17:15 +0900

    探索のバグを直した

--- src/ComputerAgent.hpp
+++ src/ComputerAgent.hpp
@@ -6,10 +6,13 @@ using Evalate = std::pair<int64_t, int32_t>;
 
 struct ComputerAgent: public Agent{
 	int maxdepth;
-	ComputerAgent(int turn, int depth = 8):
-		Agent(turn)
+	EvalPaser eval;
+	ComputerAgent(int turn,int depth = 8, std::string fname="./evl/default.evl"):
+		Agent(turn),
+		eval(fname)
 	{
 		maxdepth = depth;
+		
 	}
 	BitBoard_p think(BitBoard_p& b){
 		b->print(_turn);
@@ -22,21 +25,18 @@ struct ComputerAgent: public Agent{
 	Evalate negaScout(BitBoard_p& bd, int depth, int turn,
 			int32_t a = -INT32_MAX, int32_t b = INT32_MAX){
 
+		int nturn = (turn==BLACK)?WHITE:BLACK;
 		if( (bd->black | bd->white) == 0xffffffffffffffff){
-			return std::make_pair(0, (_turn==BLACK)?evalate_end(bd):-evalate_end(bd));
+			return std::make_pair(0, (turn==BLACK)?eval.evalate_end(bd):-eval.evalate_end(bd));
 			/* return std::make_pair(0, evalate_end(bd)); */
 		}
 
+		if(depth == 0){
+			return std::make_pair(0, (turn==BLACK)?eval.evalate(bd):-eval.evalate(bd));
+		}
 		PosVect ps = this->placeable_pos(bd, turn);
 		if(ps.size()==0){
-			return std::make_pair(0, 
-					(_turn==BLACK)?
-						-INT32_MAX:
-						INT32_MAX);
-		}
-		if(depth == 0){
-			return std::make_pair(0, (_turn==BLACK)?evalate(bd):-evalate(bd));
-			/* return std::make_pair(0, evalate(bd)); */
+			return negaScout(bd, depth-1, nturn, -b, -a);
 		}
 		// next board states is ps
 		std::vector<BitBoard_p> next_node(ps.size());
@@ -47,10 +47,10 @@ struct ComputerAgent: public Agent{
 			for(size_t i = 0; i < ps.size(); i++)
 				next_node[i] = put_white(bd, ps[i]);
 		
-		int nturn = (turn==BLACK)?WHITE:BLACK;
 		uint64_t best_pos = ps[0];
 		for(size_t i = 0; i < next_node.size(); i++){
 			auto ev = negaScout(next_node[i], depth-1, nturn, -b, -a);
+			ev.second = -ev.second;
 			if(a < ev.second){
 				best_pos = ps[i];
 				a = ev.second;
